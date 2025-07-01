8ce590f8bbf02ca712f039e53a3169bd821dd1e5
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-07-01 14:59:22 +0300

    feat: add new comand

--- git.js
+++ git.js
@@ -3,9 +3,13 @@ git init = инициализирует репозиторий
 
 git remote add origin https://github.com/ivan1986mrpl/test-repo.git = связывает локальный репозиторий с удаленным
 
+git status = показывает в какой сейчас ветке и какие файлы изменены или добавлены на локальном репозитории
 
+git add -A или <название файла> = добавляет файлы в состояние перед комитом
 
+git commit -m'init: add readme file' = добавляет контрольную точку 
 
+git push -u origin main = (origin = ссылка на ваш github) (main = название ветки, в которую пушим)
 
 
 
